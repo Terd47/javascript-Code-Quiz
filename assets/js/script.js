3282edf5e92d03ae9bcfd8200be26cf9217fb4ae
Terd47 <terdlar@gmail.com>
2020-09-19 02:55:56 -0400

    quiz questions functioning properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,136 @@
-let codeQuiz = [
+(function() 
+ {
+  var allQuestions = [{
+    question: "commonly used data types Do Not include?",
+    options: ["Strings","Booleans","Alerts","Numbers"],
+    answer: 2
+  }, {
+    question: "The condition in an if/else statement is enclosed within__________?",
+    options: ["quotes","curly brackets", "parenthesis","square brackets"],
+    answer: 2
+  }, {
+    question: "Arrays in JavaScript can be used to store _________?",
+    options: ["numbers and strings","other arrays","booleans","all of the above"],
+    answer: 3
+  },{
+    question: "String values must be enclosed within _________ when being\
+                when being assigned to variables?",
+    options: [ "comments","curly brackets","quotes","parenthesis"],
+    answer: 2
+  }, {
+    question: "A very useful tool used to during development and debugging for printing\
+               for printing content to the debugger is:?",
+    options: ["JavaScript","terminal/bash","for loops","console.log"],
+    answer: 3
+  },];
+  
+  var quesCounter = 0;
+  var selectOptions = [];
+  var quizSpace = $('#quiz');
+    
+  nextQuestion();
+    
+  $('#next').click(function () 
     {
-        question: "commonly used data types Do Not include?",
-        a: "Strings",
-        b: "Booleans",
-        c: "Alerts",
-        d: "Numbers",
-    },
+        chooseOption();
+        if (isNaN(selectOptions[quesCounter])) 
+        {
+            alert('Please select an option !');
+        } 
+        else 
+        {
+          quesCounter++;
+          nextQuestion();
+        }
+    });
+  
+  $('#prev').click(function () 
     {
-        question: "The condition in an if/else statement is enclosed within__________?",
-        a: "quotes",
-        b: "curly brackets",
-        c: "parenthesis",
-        d: "square brackets",
-    },
+        chooseOption();
+        quesCounter--;
+        nextQuestion();
+    });
+  
+  function createElement(index) 
     {
-        question: "Arrays in JavaScript can be used to store _________?",
-        a: "numbers and strings",
-        b: "other arrays",
-        c: "booleans",
-        d: "all of the above",
-    },
-    {
-        question: "String values must be enclosed within _________ when being\
-                  when being assigned to variables?",
-        a: "comments",
-        b: "curly brackets",
-        c: "quotes",
-        d: "parenthesis",
-    },
-    {
-        question: "A very useful tool used to during development and debugging for printing\
-                  for printing content to the debugger is:?",
-        a: "JavaScript",
-        b: "terminal/bash",
-        c: "for loops",
-        d: "console.log",
-    },
+        var element = $('<div>',{id: 'question'});
+        var header = $('<h2>Question No. ' + (index + 1) + ' :</h2>');
+        element.append(header);
 
-];
-const quiz = document.getElementById("quiz");
-const answerEls = document.querySelectorAll(".answer");
-const questionEl = document.getElementById("question");
-const firstBtn = document.getElementById("first-btn");
-const secondBtn = document.getElementById("second-btn");
-const thidBtn = document.getElementById("third-btn");
-const fourthBtn = document.getElementById("fourth-btn");
-const nextBtn = document.getElementById("next-button");
+        var question = $('<p>').append(allQuestions[index].question);
+        element.append(question);
 
-let myQuiz = 0;
-let score = 0;
+        var radio = radioButtons(index);
+        element.append(radio);
 
-startQuiz();
-
+        return element;
+    }
+  
+  function radioButtons(index) 
+    {
+        var radioItems = $('<ul>');
+        var item;
+        var input = '';
+        for (var i = 0; i < allQuestions[index].options.length; i++) {
+          item = $('<li>');
+          input = '<input type="radio" name="answer" value=' + i + ' />';
+          input += allQuestions[index].options[i];
+          item.append(input);
+          radioItems.append(item);
+        }
+        return radioItems;
+  }
+  
+  function chooseOption() 
+    {
+        selectOptions[quesCounter] = +$('input[name="answer"]:checked').val();
+    }
+   
+  function nextQuestion() 
+    {
+        quizSpace.fadeOut(function() 
+            {
+              $('#question').remove();
+              if(quesCounter < allQuestions.length)
+                {
+                    var nextQuestion = createElement(quesCounter);
+                    quizSpace.append(nextQuestion).fadeIn();
+                    if (!(isNaN(selectOptions[quesCounter]))) 
+                    {
+                      $('input[value='+selectOptions[quesCounter]+']').prop('checked', true);
+                    }
+                    if(quesCounter === 1)
+                    {
+                      $('#prev').show();
+                    } 
+                    else if(quesCounter === 0)
+                    {
+                      $('#prev').hide();
+                      $('#next').show();
+                    }
+                }
+              else 
+                {
+                    var scoreRslt = displayResult();
+                    quizSpace.append(scoreRslt).fadeIn();
+                    $('#next').hide();
+                    $('#prev').hide();
+                }
+        });
+    }
+  
+  function displayResult() 
+    {
+        var score = $('<p>',{id: 'question'});
+        var correct = 0;
+        for (var i = 0; i < selectOptions.length; i++) 
+        {
+          if (selectOptions[i] === allQuestions[i].answer) 
+          {
+            correct++;
+          }
+        }
+        score.append('You scored ' + correct + ' out of ' +allQuestions.length);
+        return score;
+  }
+})();
